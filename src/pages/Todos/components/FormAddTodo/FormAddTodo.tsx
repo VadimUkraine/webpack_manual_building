eb0aa71d5d35c84b8e58c4035cfe6c0e3f80fbeb
v.oleynik <v.oleynik@itransition.com>
2022-01-19 12:00:27 +0200

    test husky pre commit

--- src/pages/Todos/components/FormAddTodo/FormAddTodo.tsx
+++ src/pages/Todos/components/FormAddTodo/FormAddTodo.tsx
@@ -10,7 +10,7 @@ export const FormAddTodo: FC = () => {
   const [textValue, setTextValue] = useState('');
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault() error here;
+    e.preventDefault();
   };
 
   const handleChangeTextInput = useCallback(
